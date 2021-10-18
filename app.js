d35689b317d18c90ef748ce27bd0bfc7514e56d4
priemum <50713962+priemum@users.noreply.github.com>
2021-10-18 16:41:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
-
+          
 app.use(express.static(__dirname + '/views'))
 
 app.get('/[a-z]{0,}', (req, res) => res.render('networks'))
